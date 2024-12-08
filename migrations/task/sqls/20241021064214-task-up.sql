d5b72ab8bf5014a1b72cd4872bb2b8cc86ea137a
judyazby <judyazby520@gmail.com>
2024-12-08 20:19:32 +0800

    v7 mission 1_to_5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -201,7 +201,7 @@ VALUES
     10,
     'https://test-meeting.test.io'
 );
-/*
+
 -- ████████  █████   █    █████ 
 --   █ █   ██    █  █     █     
 --   █ █████ ███ ███      ████  
@@ -336,7 +336,7 @@ INNER JOIN "COURSE_BOOKING" cb on cp.user_id = cb.user_id
 WHERE cp.user_id = (SELECT id FROM "USER" WHERE name = '王小明')
 AND cb.cancelled_at IS NULL  -- 排除已取消的課程
 GROUP BY cp.user_id;
-
+/*
 -- ████████  █████   █     ███  
 --   █ █   ██    █  █     █     
 --   █ █████ ███ ███      ████  
